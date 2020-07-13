7e09f421e2cd0438b90adbc78023f6e9bb10e4d9
Guillaume Meurillon <guillaume.meurillon@student.howest.be>
2020-07-13 16:55:05 +0200

    Add config + modified gitignore

--- src/BikeDataProject.API/Controllers/WebRegistrationController.cs
+++ src/BikeDataProject.API/Controllers/WebRegistrationController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using System;
 using Microsoft.AspNetCore.Mvc;
 using BikeDataProject.API.Domain;
+using Microsoft.Extensions.Configuration;
 using System.Net.Http;
 
 namespace BikeDataProject.API.Controllers
@@ -11,26 +12,27 @@ namespace BikeDataProject.API.Controllers
     {
         private readonly BikeDataDbContext _dbContext;
         private readonly HttpClient _httpClient = new HttpClient();
+        private readonly IConfiguration _configuration;
 
-        public WebRegistrationController(BikeDataDbContext dbContext) => this._dbContext = dbContext;
+        public WebRegistrationController(BikeDataDbContext dbContext, IConfiguration configuration)
+        {
+            this._dbContext = dbContext;
+            this._configuration = configuration;
+        }
 
         [HttpPost("/register/strava")]
         public async Task<IActionResult> RegisterStrava(string code)
         {
             if (!String.IsNullOrWhiteSpace(code))
             {
-                var values = new Dictionary<string, string>
+                var content = new FormUrlEncodedContent(new Dictionary<string, string>
                 {
-                    {"client_id", "CLIENT_ID"},
-                    {"client_secret", "CLIENT_SECRET"},
+                    {"client_id", this._configuration.GetValue<String>("ClientID")},
+                    {"client_secret", this._configuration.GetValue<String>("ClientSecret")},
                     {"code", code},
                     {"grant_type", "authorization_code"}
-                };
-
-                var content = new FormUrlEncodedContent(values);
-
+                });
                 var response = await this._httpClient.PostAsync("https://www.strava.com/oauth/token", content);
-
                 var responseString = await response.Content.ReadAsStringAsync();
                 return this.Ok(String.Format("response is {0}", responseString));
             }
